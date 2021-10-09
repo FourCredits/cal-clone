b33138919ceb55092c401c22fa4276cb1cc2a2a4
Erik <erik.grundy@gmail.com>
2021-10-10 11:32:18 +1300

    Formatting changes

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,8 @@ main = do
   today <- getCurrentDay
   let (y, m, _) = toGregorian today
       argParser = Args <$> (monthP m) <*> (yearP y)
-      parser = info (argParser <**> helper) (fullDesc <> progDesc "Print a calendar")
+      parser =
+        info (argParser <**> helper) (fullDesc <> progDesc "Print a calendar")
   Args m y <- execParser parser
   printMonth today y m
 
@@ -23,10 +24,23 @@ data Args =
   deriving (Show)
 
 yearP :: Integer -> Parser Integer
-yearP y = option auto ( long "year" <> short 'y' <> metavar "Y" <> showDefault <> value y <> help "Print Y year")
+yearP y = option auto
+  (  long "year"
+  <> short 'y'
+  <> metavar "Y"
+  <> showDefault
+  <> value y
+  <> help "Print Y year"
+  )
 
 monthP :: Int -> Parser Int
-monthP m = option auto ( long "month" <> short 'm' <> metavar "M" <> showDefault <> value m <> help "Print M month" )
+monthP m = option auto
+  (  long "month"
+  <> short 'm'
+  <> metavar "M"
+  <> showDefault
+  <> value m
+  <> help "Print M month" )
 
 getCurrentDay :: IO Day
 getCurrentDay = localDay . zonedTimeToLocalTime <$> getZonedTime
